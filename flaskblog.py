bcf387a855b87f2b26196b8d121d10f405b370d6
prateeksawhney97 <prateek.sawhney97@gmail.com>
2019-09-20 22:35:42 +0530

    using dictionaries

--- flaskblog.py
+++ flaskblog.py
@@ -1,16 +1,36 @@
 from flask import Flask, escape, request, render_template
 
 app = Flask(__name__)
+
+
+posts = [
+    {
+    'author':'Prateek Sawhney',
+    'title':'Blog post 1',
+    'content':'Blog 1 content',
+    'date_posted':'April 19, 2019'
+    },
+    {
+    'author':'Sameeksha Sawhney',
+    'title':'Blog post 2',
+    'content':'Blog 2 content',
+    'date_posted':'April 29, 2019'
+    }
+	
+
+]
+
+
 #setting variable app to the instance of this flask class
 @app.route('/')
 @app.route('/home')
 #these 2 routes are handled by the same function
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
 if __name__=="__main__":
     app.run(debug=True)
