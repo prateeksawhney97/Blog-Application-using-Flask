b9cf6068f5ad0df5b8ea3f6371d7f7b7892de656
prateeksawhney97 <prateek.sawhney97@gmail.com>
2019-09-20 22:08:02 +0530

    rendering template

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,16 @@
-from flask import Flask, escape, request
+from flask import Flask, escape, request, render_template
 
 app = Flask(__name__)
 #setting variable app to the instance of this flask class
 @app.route('/')
-def hello():
-    return "<h1>Home Page</h1>"
+@app.route('/home')
+#these 2 routes are handled by the same function
+def home():
+    return render_template('home.html')
 
 @app.route('/about')
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html')
 
 if __name__=="__main__":
     app.run(debug=True)
