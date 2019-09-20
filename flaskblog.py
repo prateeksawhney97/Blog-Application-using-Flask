982137fd680a3f72dac60a4b379096106ba96a1e
prateeksawhney97 <prateek.sawhney97@gmail.com>
2019-09-20 21:21:16 +0530

    running the flask app

--- flaskblog.py
+++ flaskblog.py
@@ -1,8 +1,7 @@
 from flask import Flask, escape, request
 
 app = Flask(__name__)
-
+#setting variable app to the instance of this flask class
 @app.route('/')
 def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+    return "Hello World!"
